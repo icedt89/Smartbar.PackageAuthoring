070fc38b567044f79b0cbae8373bcd6e5be72561
Jan Hafner <icedt89@gmail.com>
2016-12-18 13:29:04 +0100

    - Renamed Server Project

--- Source/Smartbar.Server/Properties/AssemblyInfo.cs
+++ Source/Smartbar.Server/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Smartbar.Server.NuGet")]
+[assembly: AssemblyTitle("Smartbar Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Smartbar.Server.NuGet")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Smartbar")]
+[assembly: AssemblyCopyright("Copyright © Jan Hafner")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
